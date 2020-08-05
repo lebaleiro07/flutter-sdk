f4907974c75cf50f8c1d81575c79a54c1705f420
Leticia <Leticia Baleiro>
2020-08-05 17:41:09 -0300

    feat(profile): add playlist endpoint

---
+++ lib/core/api/endpoints/playlist_endpoint.dart
@@ -0,0 +1,20 @@
+import 'package:get_it/get_it.dart';
+import 'package:music_playce_sdk/core/api/environment.dart';
+
+class PlaylistEndpoint {
+  static final _env = GetIt.instance<Environment>();
+
+  static String getAPlaylist(String playlistId) =>
+      "${_env.apiHost}/v1/playlists/$playlistId";
+
+  static String addAPostToPlaylist(String playlistId) =>
+      "${_env.apiHost}/v1/playlists/$playlistId";
+
+  static String deleteAPostFromAPlaylist(String playlistId, String postId) =>
+      "${_env.apiHost}/v1/playlists/$playlistId/$postId";
+
+  static String createPlaylist = "${_env.apiHost}/v1/playlists";
+
+  static String deleteAPlaylist(String playlistId) =>
+      "${_env.apiHost}/v1/playlists/$playlistId";
+}
