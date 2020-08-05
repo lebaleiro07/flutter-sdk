ac91567f9d9a97a321a0e9cd6ff7341c7933bd78
Leticia <Leticia Baleiro>
2020-08-05 17:42:22 -0300

    feat(profile): add playlists models

---
+++ lib/core/api/models/playlists/playlist_response.model.dart
@@ -0,0 +1,65 @@
+import 'package:music_playce_sdk/core/api/models/posts/posts.model.dart';
+
+class PlaylistResponse {
+  String idPlaylist;
+  String idProfile;
+  String playlistName;
+  String description;
+  String idPicture;
+  bool isPublic;
+  bool isEditable;
+  bool visible;
+  String typeSource;
+  int length;
+  int duration;
+  List<Post> posts;
+
+  PlaylistResponse({
+    this.idPlaylist,
+    this.idProfile,
+    this.playlistName,
+    this.description,
+    this.idPicture,
+    this.isPublic,
+    this.isEditable,
+    this.visible,
+    this.typeSource,
+    this.length,
+    this.duration,
+    this.posts,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      "_id": idPlaylist,
+      "id_profile": idProfile,
+      "name": playlistName,
+      "description": description,
+      "id_picture": idPicture,
+      "is_public": isPublic,
+      "is_editable": isEditable,
+      "visible": visible,
+      "type_source": typeSource,
+      "length": length,
+      "duration": duration,
+      "posts": posts,
+    };
+  }
+
+  factory PlaylistResponse.fromJson(dynamic data) {
+    return PlaylistResponse(
+      idPlaylist: data['_id'],
+      idProfile: data['id_profile'],
+      playlistName: data['name'],
+      description: data['description'],
+      idPicture: data['id_picture'],
+      isPublic: data['is_public'],
+      isEditable: data['is_editable'],
+      visible: data['visible'],
+      typeSource: data['type_source'],
+      length: data['length'],
+      duration: data['duration'],
+      posts: data['posts'],
+    );
+  }
+}
