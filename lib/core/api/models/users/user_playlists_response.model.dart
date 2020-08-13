7229a3e3f088fda54098eef42881f995cb1e4758
Leticia <Leticia Baleiro>
2020-08-13 18:02:28 -0300

    feat(playlist): update user playlist model

--- lib/core/api/models/users/user_playlists_response.model.dart
+++ lib/core/api/models/users/user_playlists_response.model.dart
@@ -1,6 +1,6 @@
-import 'package:music_playce_sdk/core/api/models/posts/picture.model.dart';
+import 'dart:convert';
 
-import '../posts/posts.model.dart';
+import 'package:music_playce_sdk/core/api/models/posts/picture.model.dart';
 
 class UserPlaylists {
   String idPlaylist;
@@ -23,29 +23,32 @@ class UserPlaylists {
     this.picture,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      "_id": idPlaylist,
-      "id_profile": idProfile,
-      "name": playlistName,
-      "duration": duration,
-      "length": length,
-      "is_editable": isEditable,
-      "is_public": isPublic,
-      "picture": picture,
-    };
-  }
+  factory UserPlaylists.fromJson(String str) =>
+      UserPlaylists.fromMap(json.decode(str));
 
-  factory UserPlaylists.fromJson(dynamic data) {
-    return UserPlaylists(
-      idPlaylist: data['_id'],
-      idProfile: data['id_profile'],
-      playlistName: data['name'],
-      duration: data['duration'].toInt(),
-      length: data['length'],
-      isEditable: data['is_editable'],
-      isPublic: data['is_public'],
-      picture: data['picture'],
-    );
-  }
+  String toJson() => json.encode(toMap());
+
+  factory UserPlaylists.fromMap(Map<String, dynamic> json) => new UserPlaylists(
+        idPlaylist: json['_id'] == null ? null : json['_id'],
+        idProfile: json['id_profile'] == null ? null : json['id_profile'],
+        playlistName: json['name'] == null ? null : json['name'],
+        duration:
+            json['duration'].toInt() == null ? null : json['duration'].toInt(),
+        length: json['length'] == null ? null : json['length'],
+        isEditable: json['is_editable'] == null ? null : json['is_editable'],
+        isPublic: json['is_public'] == null ? null : json['is_public'],
+        picture: json['picture'] == null ? null : json['picture'],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "_id": idPlaylist == null ? null : idPlaylist,
+        "id_profile": idProfile == null ? null : idProfile,
+        "name": playlistName == null ? null : playlistName,
+        "duration": duration == null ? null : duration,
+        "length": length == null ? null : length,
+        "is_editable": isEditable == null ? null : isEditable,
+        "is_public": isPublic == null ? null : isPublic,
+        "picture": picture == null ? null : picture,
+      };
+
 }
