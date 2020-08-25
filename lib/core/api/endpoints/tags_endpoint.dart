39f13192e0435cc69c15e7a73908dcacfd2ee2ec
Pedro Lemos <lemozxpedro@gmail.com>
2020-08-25 12:42:39 -0300

    feat: added endpoint for tags

---
+++ lib/core/api/endpoints/tags_endpoint.dart
@@ -0,0 +1,17 @@
+import 'package:get_it/get_it.dart';
+
+import '../environment.dart';
+
+class TagsEndpoint {
+  static final _env = GetIt.instance<Environment>();
+
+  static final String getAllTags = '${_env.apiHost}/v1/tags/';
+
+  static String getTagById(String tagId) => '${_env.apiHost}/v1/tags/$tagId';
+
+  static String followTag(String tagId) =>
+      '${_env.apiHost}/v1/tags/$tagId/follow';
+
+  static String getAllPostByTagId(String tagId) =>
+      '${_env.apiHost}/v1/tags/$tagId/posts';
+}
